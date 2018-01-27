ade7f3f844524f7fd86191274df928505b793967
Tom Houlé <tom@tomhoule.com>
2018-01-27 12:37:27 +0100

    Create error type

---
+++ src/error.rs
@@ -0,0 +1,7 @@
+#[derive(Debug, Fail)]
+pub enum Error {
+    #[fail(display="Database cannot be reached")]
+    DatabaseUnreachable,
+    #[fail(display="Invalid input")]
+    InvalidInput,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,63 @@
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate failure;
 extern crate futures;
 extern crate grpcio;
 extern crate protobuf;
+extern crate r2d2;
 extern crate uuid;
 
 mod db_schema;
+mod error;
 mod rpc;
 
 use rpc::yacchauyo_grpc::Yacchauyo;
-use std::io::prelude::*;
 use grpcio::{Environment, RpcContext, ServerBuilder, UnarySink};
 use futures::Future;
 use std::sync::Arc;
-use futures::sync::oneshot;
-use std::thread;
+use diesel::prelude::*;
 
 #[derive(Clone)]
-struct Server;
+struct Server {
+    pool: r2d2::Pool<diesel::r2d2::ConnectionManager<PgConnection>>,
+}
+
+impl Server {
+    fn new() -> Self {
+        unimplemented!()
+    }
+}
 
 impl Yacchauyo for Server {
     fn texts_index(
         &self,
-        ctx: ::grpcio::RpcContext,
+        ctx: RpcContext,
         _req: ::rpc::yacchauyo::TextsQuery,
-        sink: ::grpcio::UnarySink<rpc::yacchauyo::Texts>
+        sink: UnarySink<rpc::yacchauyo::Texts>
     ) {
         let mut response = ::rpc::yacchauyo::Texts::new();
         let mut first_text = ::rpc::yacchauyo::Text::new();
         first_text.set_title("Ethica more geometrico demonstrata".to_string());
         response.texts.push(first_text);
         let f = sink.success(response)
-            .map_err(|err| panic!("{:?}", err));
+            .map_err(|_err| ());
         ctx.spawn(f);
     }
 }
 
 pub fn start() {
     let env = Arc::new(Environment::new(4));
-    let service = ::rpc::yacchauyo_grpc::create_yacchauyo(Server);
-    println!("Got this far 97");
+    let service = ::rpc::yacchauyo_grpc::create_yacchauyo(Server::new());
     let mut server = ServerBuilder::new(env)
         .register_service(service)
         .bind("127.0.0.1", 4443)
         .build()
         .unwrap();
     server.start();
-    println!("Got thi far 99");
-    // for &(ref host, port) in server.bind_addrs() {
-    //     println!("listening on {}:{}", host, port);
-    // }
+    for &(ref host, port) in server.bind_addrs() {
+        println!("listening on {}:{}", host, port);
+    }
     // let (tx, rx) = oneshot::channel();
     // thread::spawn(move || {
     //     println!("Press ENTER to exit...");
@@ -58,6 +66,5 @@ pub fn start() {
     // });
     // let _ = rx.wait();
     loop {}
-    println!("Got this far 101");
     let _ = server.shutdown().wait();
 }
