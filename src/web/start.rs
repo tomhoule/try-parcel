d797b772f28e74525a7c5364d79dfb23244d108e
Tom Houlé <tom@tomhoule.com>
2018-02-08 22:30:21 +0100

    Add new text form stub

--- src/web/start.rs
+++ src/web/start.rs
@@ -22,6 +22,15 @@ fn index() -> Index<'static> {
     Index { name: "meow", _parent: Base }
 }
 
+#[derive(Template)]
+#[template(path = "t/new.html")]
+struct TextNew { _parent: Base }
+
+#[get("/t/new")]
+fn t_new() -> TextNew {
+    TextNew { _parent: Base }
+}
+
 pub fn start() -> rocket::Rocket {
     use_default_config!();
     let config = config::Config::generate().unwrap();
@@ -29,8 +38,13 @@ pub fn start() -> rocket::Rocket {
     let pool: r2d2::Pool<ConnectionManager<PgConnection>> =
         r2d2::Pool::new(pool_manager).expect("Failed to create a database connection pool");
 
+    let routes = routes![
+        index,
+        t_new,
+    ];
+
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes)
         .manage(pool)
 }
 
@@ -48,5 +62,15 @@ mod tests {
         assert_eq!(res.status(), Status::Ok);;
         let body_string = res.body_string().unwrap();
         assert!(body_string.contains("meow"), body_string);
+    }
+
+    #[test]
+    fn t_new_works() {
+        let client = Client::new(start()).unwrap();
+        let req = client.get("/t/new");
+        let mut res = req.dispatch();
+        assert_eq!(res.status(), Status::Ok);;
+        let body_string = res.body_string().unwrap();
+        assert!(body_string.contains("<form"), body_string);
     }
 }
