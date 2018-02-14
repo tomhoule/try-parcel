6cf4a25b527cf7a05064c103eeed3173068ba077
Tom Houlé <tom@tomhoule.com>
2018-02-14 21:58:52 +0100

    Implement command line argument passing
    Select rpc or web server. Yay.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 extern crate dotenv;
 extern crate yacchauyo;
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "server")]
+enum Command {
+    #[structopt(name = "rpc")]
+    Rpc,
+    #[structopt(name = "web")]
+    Web,
+}
 
 fn main() {
     dotenv::dotenv().ok();
-    // yacchauyo::start_rpc();
-    yacchauyo::start_web();
+    match Command::from_args() {
+        Command::Rpc => {
+            yacchauyo::start_rpc();
+        },
+        Command::Web => {
+            yacchauyo::start_web();
+        }
+    }
 }
